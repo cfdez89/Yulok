2c02b1bf679be931971858f6f4ff1e4750d85a9a
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-11-02 17:32:30 -0600

    se agregan estados para vistas

--- presentation/routes/configuration.js
+++ presentation/routes/configuration.js
@@ -20,22 +20,27 @@ angular
     
     routes.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-    function routes($stateProvider, $urlRouterProvider) {
+    function routes($stateProvider, $urlRouterProvider) { console.log('routes');
         $urlRouterProvider
             .otherwise('/');
         $stateProvider
 		    .state('no-logged', {
 			    abstract: true,
 			    url: '',
-			    templateUrl: '/presentation/views/no-logged.html',
+			    templateUrl: '/presentation/components/layout/noLogged/views/noLogged.html',
 			    controller: 'noLoggedCtl as vm'
             })
-            .state('home', {
+            .state('login', {
                 url: '/', 
                 parent: 'no-logged',
-                templateUrl:'/presentation/views/login.html',
-                controller: ' as vm'
+                templateUrl:'/presentation/components/login/views/login.html',
+                controller: 'loginCtl as vm'
             })
+            .state('about',{
+                url: '/about', 
+                parent: 'no-logged',
+                templateUrl: '/presentation/components/about/views/about.html'
+            });
 
     };
 
