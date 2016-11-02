ea5338945b30ee3c81d228024cf676d585b66888
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-11-02 11:55:45 -0600

    se definen las constantes para acceder al api de yurok

---
+++ presentation/constants/constants.js
@@ -0,0 +1,13 @@
+/*
+ *Yulok app
+ *By Carlos Fernandez Jimenez
+ *App constants
+ */
+ 
+(function() {
+    'use strict';
+    angular
+        .module('yulok')
+        .constant('API_URL', 'http://yulokcr.com');
+
+})();
