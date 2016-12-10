692b85819ad309d2fa27467c2c8d6f05879a2077
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-12-10 04:04:59 -0600

    validaciones

--- presentation/services/validationService.js
+++ presentation/services/validationService.js
@@ -16,11 +16,26 @@
             var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
             return expr.test(pEmail) ? true: false;
         };
+
+        function comparePassword(pPassword, pConfirmPassword) {
+            return pPassword === pConfirmPassword ? true: false;
+        };
+
+        function passwordSize(pPassword) {
+            return pPassword.length > 6 ? true
+                                        : false;
+        };
 		
         return {
             isEmail: function(pEmail) {
                 return email(pEmail);             
-            }   
+            },
+            comparePassword: function(pPassword, pConfirmPassword) {
+                return comparePassword(pPassword, pConfirmPassword);             
+            },
+            isPasswordMin: function(pPassword) {
+                return passwordSize(pPassword);             
+            }    
         };   
     };
 
